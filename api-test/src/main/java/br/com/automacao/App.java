b664fd4034ba8c234fca0dab29c2a8b6c92fe00d
marcus1708 <marcusviniciusbordin@gmail.com>
2026-02-23 13:08:39 -0300

    docs

---
+++ api-test/src/main/java/br/com/automacao/App.java
@@ -0,0 +1,13 @@
+package br.com.automacao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
